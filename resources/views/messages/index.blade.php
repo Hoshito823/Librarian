d3653ae9c74b4abb8056dcdba476e49f655d8b1b
Hoshito823 <otihsoh823@gmail.com>
2019-12-20 17:46:28 +0000

    commit for completion of chat design.

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -5,18 +5,22 @@
 @section('content')
 <div class="container">
     <h2>Chat Room</h2>
-    <div class="col-md-6 mx-auto">
+    <div class="col-md-8 mx-auto">
         @foreach($conversation as $message)
           @if($message->from_user_id == $me->id)
               {{-- Show My Messages --}}
-              <div class="message" style="text-align:right">
-                  <p style="color:yellow">{{ $me->name }}</p>
-                  <p style="color:yellow">{{ $message->body }}</p>
+              <div class="message-container-right">
+                  <p class="username">{{ $me->name }}</p>
+                  <div class="balloon1-right">
+                      <p>{{ $message->body }}</p>
+                  </div>
               </div>
         　@else
-        　    <div class="message">
-        　        <p>{{ $message->from_user->name }}</p>
-                 <p>{{ $message->body }}</p>
+        　    <div class="message-containe-left">
+        　        <p class="username">{{ $message->from_user->name }}</p>
+            　    <div class="balloon1-left">
+                     <p>{{ $message->body }}</p>
+                 </div>
              </div>
           @endif
         @endforeach
